7635e2e2497d8b9d172e19e29ffbfccc94dfd651
muneer02 <41252573+muneer02@users.noreply.github.com>
2019-07-16 20:33:59 +0530

    Create mail.php

---
+++ mail.php
@@ -0,0 +1,32 @@
+<html>
+<body>
+
+<h2>Send e-mail to someone@example.com:</h2>
+
+<form  action="<?php echo $_SERVER['PHP_SELF']; ?>"  method="post" >
+Subject:<br>
+<input type="text" name="subject"><br>
+Message:<br>
+<input type="text" name="msg"><br>
+E-mail:<br>
+<input type="text" name="mail"><br><br>
+
+<button type="submit"  name="submit" value="submit">SEND MAIL</button>
+
+</form>
+
+</body>
+</html>
+
+
+
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+$sub = $_POST["subject"] ;
+$msg = $_POST["msg"];
+$rec = $_POST["mail"];
+mail($rec,$sub,$msg);
+}
+?>
